61f784c8c67f230d4247b26c8712894ffffe6d92
Benjamin James <benjames@mit.edu>
2022-09-18 02:16:39 -0400

    rest 20% are having brain

---
+++ R/colors.R
@@ -0,0 +1,19 @@
+
+
+# Color schemes for cell types
+#' @export
+colors = list(celltypes=c("Ast"="#D6242A",
+                          "Exc"="#45A23A",
+                          "Inh"="#9ED97A",
+                          "Mic"="#BE9FCE",
+                          "Oli"="#F4A85E",
+                          "Opc"="#A14E2D",
+                          "Spn"="#F59695",
+                          "Vas"="#A7A8AA"),
+              regions=c("AG"="#8DD3C7",
+                        "MT"="#80B1D3",
+                        "NAc"="#32768C",
+                        "PFC"="#FDB462",
+                        "EC"="#FFED6F",
+                        "HC"="#BEBADA",
+                        "TH"="#B3DE69"))

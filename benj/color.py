bff33599a927641efd09d09261829ef7858fa40d
Benjamin James <benjames@mit.edu>
2022-09-27 01:09:32 -0400

    benj colors

--- benj/color.py
+++ benj/color.py
@@ -3,7 +3,10 @@
 def cm_benj(base="viridis", count=100, zero=[0.4, 0.4, 0.4, 1]):
     from matplotlib.cm import get_cmap
     import matplotlib.colors as mcolors
+    import matplotlib
     import numpy as np
     cm = get_cmap(base)(np.linspace(0, 1, count-1))
     colors = np.vstack((zero, cm))
-    return mcolors.LinearSegmentedColormap.from_list('benj', colors)
+    my_cm = LinearSegmentedColormap.from_list('benj', colors)
+    matplotlib.colormaps.register(my_cm)
+    return "benj"

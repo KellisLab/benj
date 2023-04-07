8205fd98c42db150c436017c0a2a596e715a4b1d
Benjamin James <benjames@mit.edu>
2023-04-07 18:12:05 -0400

    Added aggregate_anndata script. Added TSS to rna H5AD creation.

---
+++ scripts/aggregate_anndata.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+def add_cell_cycle_info(adata, cell_cycle):
+    return 0
+
+def run(metadata, output, directory=".", sample_key="Sample", cell_cycle=None, gtf=None, min_cells_per_sample:int=30, compression:int=9, use_scrublet:bool=True):
+    import os
+    from tqdm.auto import tqdm
+    import pandas as pd
+    import numpy as np
+    import scanpy as sc
+    import benj
+    md = pd.read_csv(metadata, sep="\t", index_col=0)
+    tbl = []
+    sw = benj.stopwatch()
+    bad = []
+    with sw("Reading H5AD files"):
+        for sample in tqdm(md.index.values):
+            fname = os.path.join(directory, "%s.h5ad" % sample)
+            if os.path.isfile(fname):
+                adata = sc.read(fname)
+                for cn in md.columns:
+                    adata.obs[cn] = md[cn].values
+                if adata.shape[0] >= min_cells_per_sample:
+                    tbl[sample] = adata
+                else:
+                    bad.append(sample)
+    if bad:
+        print("Bad samples: ", ",".join(bad))
+    with sw("Concatenating AnnData objects"):
+        adata = anndata.concat(tbl, merge="same", uns_merge="same")
+    with sw("Calculating statistics"):
+        sc.pp.calculate_qc_metrics(adata, qc_vars=qc_vars, inplace=True, percent_top=[])
+    if use_scrublet:
+        with sw("Scrublet"):
+            sc.external.pp.scrublet(adata, batch_key="Sample")
+    with sw("Writing H5AD"):
+        adata.write_h5ad(output, compression="gzip", compression_opts=compression)
+
+if __name__ == "__main__":
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-m", "--metadata", required=True, help="Pre-formatted metadata TSV with index as sample names")
+        ap.add_argument("-d", "--directory", default=".", help="Directory with {Sample}.h5ad files inside")
+        ap.add_argument("-o", "--output", required=True)
+        ap.add_argument("--qc-vars", nargs="+")
+        ap.add_argument("--min-cells-per-sample", type=int, default=30)
+        ap.add_argument("--use-scrublet", dest="scrublet", action="store_true")
+        ap.add_argument("--no-use-scrublet", dest="scrublet", action="store_false")
+        ap.add_argument("--compression", type=int, default=9)
+        ap.set_defaults(scrublet=True)
+        args = vars(ap.parse_args())
+        run(**args)
--- scripts/h5ad_from_cellranger_rna.py
+++ scripts/h5ad_from_cellranger_rna.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
-def run(h5, output, sample:str=None, use_muon:bool=True, compression:int=9, **kwargs):
+def get_tss(tss:str):
+    import pandas as pd
+    tss = pd.read_csv(tsv, sep="\t", header=None)
+    tss.columns = ["Chromosome", "Start", "End", "gene_id", "score", "strand"]
+    df = tss.groupby(["Chromosome", "gene_id", "strand"]).agg(left=("Start", "min"),
+                                                              right=("End", "max")).reset_index()
+    df["interval"] = df["Chromosome"] + ":" + df["left"].astype(str) + "-" + df["right"].astype(str)
+    df.index = df["gene_id"].values
+    return df
+
+def run(h5, output, sample:str=None, compression:int=9, tss:str=None, **kwargs):
     import os
     from warnings import warn
     import numpy as np
@@ -26,12 +36,11 @@ def run(h5, output, sample:str=None, use_muon:bool=True, compression:int=9, **kw
             vdata.obs.index = [x.split(":")[1] + "-1" for x in vdata.obs_names]
             vdata.var_names_make_unique()
     with sw("Reading H5"):
-        if use_muon:
-            import muon as mu
-            adata = mu.read_10x_h5(h5).mod["rna"]
-        else:
-            adata = sc.read_10x_h5(h5)
+        adata = sc.read_10x_h5(h5, gex_only=True)
         adata.var_names_make_unique()
+    if tss is not None and os.path.exists(tss):
+        tss = get_tss(tss)
+        adata.var["interval"] = [tss["interval"].get(g, "NA") for g in adata.var["gene_ids"]]
     if vdata is not None:
         with sw("Adding spliced/unspliced counts"):
             I = np.intersect1d(vdata.obs_names, adata.obs_names)
@@ -45,11 +54,10 @@ def run(h5, output, sample:str=None, use_muon:bool=True, compression:int=9, **kw
         adata.obs[kwargs.get("sample_name", "Sample")] = sample
     with sw("Converting counts to int"):
         adata.X = adata.X.astype(np.int32)
-    adata.var["mt"] = adata.var_names.str.startswith("MT-")
-    adata.var["ribo"] = adata.var_names.str.startswith(("RPS", "RPL"))
-    adata.var["hbox"] = adata.var_names.str.contains(("^HB[^(P)]"))
+    adata.var["mt"] = adata.var_names.str.startswith(("MT-", "mt-"))
+    adata.var["ribo"] = adata.var_names.str.startswith(("RPS", "RPL", "Rps", "Rpl"))
     with sw("Calculating QC metrics"):
-        sc.pp.calculate_qc_metrics(adata, qc_vars=["mt", "ribo", "hbox"], inplace=True, percent_top=[])
+        sc.pp.calculate_qc_metrics(adata, qc_vars=["mt", "ribo"], inplace=True, percent_top=[])
     with sw("Writing H5AD"):
         adata.uns = benj.convert_dict(adata.uns)
         adata.write_h5ad(output, compression="gzip", compression_opts=compression)
@@ -62,6 +70,7 @@ if __name__ == "__main__":
     ap.add_argument("--sample-name", default="Sample")
     ap.add_argument("--output", required=True)
     ap.add_argument("--compression", type=int, default=9)
+    ap.add_argument("--tss", help="TSS.bed file from refdata-cellranger-arc-*/regions/tss.bed")
     ap.add_argument("--use-muon", dest="use_muon", action="store_true")
     ap.add_argument("--no-use-muon", dest="use_muon", action="store_false")
     ap.set_defaults(use_muon=True)

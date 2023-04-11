db9c4ed115fc993c64a71032173f3689d91b4d4b
Benjamin James <benjames@mit.edu>
2023-04-11 18:35:43 -0400

    Added plotting to script

--- scripts/estimate_gene_accessibility.py
+++ scripts/estimate_gene_accessibility.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from typing import Iterable
 def estimate_and_rank(adata, gtf:str,
                       min_upstream:int=1000,
                       max_upstream:int=100000,
@@ -12,6 +12,7 @@ def estimate_and_rank(adata, gtf:str,
                       layer:str=None,
                       groupby:str=None,
                       method:str="wilcoxon",
+                      plot:Iterable[str]=None,
                       **kwargs):
     import scanpy as sc
     from benj.gene_estimation import estimate_genes_archr
@@ -24,6 +25,10 @@ def estimate_and_rank(adata, gtf:str,
     sc.pp.log1p(gdata)
     if groupby is not None:
         sc.tl.rank_genes_groups(gdata, groupby=groupby, method=method, pts=True)
+    if plot is None:
+        plot = []
+    for item in plot:
+        sc.pl.umap(gdata, color=item, save="_%s.png" % item)
     return gdata
 
 if __name__ == "__main__":
@@ -32,8 +37,10 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--input", dest="h5ad", required=True)
     ap.add_argument("-o", "--output", required=True)
-    ap.add_argument("--gtf", required=True)
-    ap.add_argument("--groupby", default=None)
+    ap.add_argument("--gtf", required=True, help="GTF file for gene annotation")
+    ap.add_argument("--groupby", default=None, help=".obs group for ranking genes")
+    ap.add_argument("--plot", nargs="+", help="Items to plot in UMAP")
+    ap.add_argument("--method", default="wilcoxon", help="Method for rank_genes_groups")
     ap.add_argument("--max-upstream", type=int, default=100000)
     ap.add_argument("--max-downstream", type=int, default=100000)
     ap.add_argument("--min-upstream", type=int, default=1000)
@@ -42,7 +49,6 @@ if __name__ == "__main__":
     ap.add_argument("--gene-downstream", type=int, default=0)
     ap.add_argument("--target-sum", type=int, default=10000)
     ap.add_argument("--gene-scale-factor", type=float, default=5.)
-    ap.add_argument("--method", default="wilcoxon")
     ap.add_argument("--layer", type=str, default=None)
     ap.add_argument("--compression", type=int, default=9)
     args = benj.parse_args(ap, ["log", "scanpy", "anndata"])

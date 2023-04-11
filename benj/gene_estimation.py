71999838e2c91be2ee704246b4d36ddfa9920c59
Benjamin James <benjames@mit.edu>
2023-04-11 19:50:43 -0400

    Added timers

--- benj/gene_estimation.py
+++ benj/gene_estimation.py
@@ -23,59 +23,66 @@ def estimate_genes_archr(adata, gtf:str,
     import pyranges
     import anndata
     import scanpy as sc
-    gf = pyranges.read_gtf(gtf).df
-    gf = gf.loc[gf["Feature"] == "gene", :]
-    gf["gene_length"] = gf["End"] - gf["Start"]
-    ### scale by inverse gene length
-    gs = 1/gf["gene_length"].values
-    ### min max scale
-    gs = (gs - np.min(gs)) / (np.max(gs) - np.min(gs))
-    ### expand to 1 to GSF range. But take log, so log1p((gs-1)*s) = log(1 + (gs - 1) * s)
-    gf["log_gene_scale"] = np.log1p((gene_scale_factor - 1) * gs)
-    gf.index = gf["gene_id"].values
-    gf = _extend_pre_ranges(gf, upstream=gene_upstream, downstream=gene_downstream)
-    gf["MinStart"] = gf["Start"].values
-    gf["MinEnd"] = gf["End"].values
-    gf = _extend_pre_ranges(gf, upstream=min_upstream, downstream=min_downstream, start="MinStart", end="MinEnd")
-    gr = pyranges.from_dict({"Chromosome": gf["Chromosome"],
-                             "Start": gf["Start"],
-                             "End": gf["End"],
-                             "gene_id": gf["gene_id"]})
-    mingr = pyranges.from_dict({"Chromosome": gf["Chromosome"],
-                                "Start": gf["MinStart"],
-                                "End": gf["MinEnd"],
-                                "gene_id": gf["gene_id"]})
+    from .timer import template
+    sw = template()
+    with sw("Reading GTF"):
+        gf = pyranges.read_gtf(gtf).df
+        gf = gf.loc[gf["Feature"] == "gene", :]
+    with sw("Extending ranges"):
+        gf["gene_length"] = gf["End"] - gf["Start"]
+        ### scale by inverse gene length
+        gs = 1/gf["gene_length"].values
+        ### min max scale
+        gs = (gs - np.min(gs)) / (np.max(gs) - np.min(gs))
+        ### expand to 1 to GSF range. But take log, so log1p((gs-1)*s) = log(1 + (gs - 1) * s)
+        gf["log_gene_scale"] = np.log1p((gene_scale_factor - 1) * gs)
+        gf.index = gf["gene_id"].values
+        gf = _extend_pre_ranges(gf, upstream=gene_upstream, downstream=gene_downstream)
+        gf["MinStart"] = gf["Start"].values
+        gf["MinEnd"] = gf["End"].values
+        gf = _extend_pre_ranges(gf, upstream=min_upstream, downstream=min_downstream, start="MinStart", end="MinEnd")
+        gr = pyranges.from_dict({"Chromosome": gf["Chromosome"],
+                                 "Start": gf["Start"],
+                                 "End": gf["End"],
+                                 "gene_id": gf["gene_id"]})
+        mingr = pyranges.from_dict({"Chromosome": gf["Chromosome"],
+                                    "Start": gf["MinStart"],
+                                    "End": gf["MinEnd"],
+                                    "gene_id": gf["gene_id"]})
     ##
     ## Now, get peak ranges (pr) from peak frame (pf)
     ##
-    if peak_column is None:
-        pstr = adata.var_names.values
-    else:
-        pstr = adata.var[peak_column].values
-    pf = pd.DataFrame([x.replace(":", "-", 1).split("-") for x in pstr], columns=["Chromosome", "Start", "End"], index=adata.var_names)
-    pr = pyranges.from_dict({"Chromosome": pf["Chromosome"], "Start": pf["Start"], "End": pf["End"], "peak_name": pf.index.values})
-    ## Once peak ranges are gathered, find intersecting gene bodies:
-    inter_df = pr.join(gr).df.loc[:, ["peak_name", "gene_id"]]
-    inter_df["Distance"] = 0
-    ## Then, find genes with minimum upstream/downstream distance away
-    min_df = pr.join(mingr).df.loc[:, ["peak_name", "gene_id"]]
-    ## diff. is accurate, unless overlapping intervals. Do not need to worry, as duplicates from inter_df will take care
-    diff = pf.loc[min_df["peak_name"].values, ["Start", "Start", "End", "End"]].values.astype(int) - gf.loc[min_df["gene_id"].values, ["Start", "End", "Start", "End"]].values.astype(int)
-    min_df["Distance"] = np.abs(diff).min(1) + 1
-    ## Finally, find distal. Only need nearest gene
-    distance_df = pr.nearest(gr).df.loc[:, ["peak_name", "gene_id", "Distance"]]
-    ## Concat such that 1) prioritized intersections, then 2) minimum distance away, then 3) distal
-    df = pd.concat([inter_df, min_df, distance_df]).drop_duplicates(["peak_name", "gene_id"])
-    df["weight"] = np.exp(-1 - np.abs(df["Distance"]) / 5000. + gf.loc[df["gene_id"].values, "log_gene_scale"].values)
-    S = scipy.sparse.csr_matrix((df["weight"].values,
-                                 (pf.index.get_indexer(df["peak_name"].values),
-                                  gf.index.get_indexer(df["gene_id"].values))),
-                                shape=(pf.shape[0], gf.shape[0]))
-    if layer is not None and layer in adata.layers:
-        X = adata.layers[layer]
-    else:
-        X = adata.X
-    gdata = anndata.AnnData(X.dot(S), obs=adata.obs, var=gf.loc[:, ["gene_id", "gene_name"]], dtype=np.float32)
+    with sw("Extracting peak ranges"):
+        if peak_column is None:
+            pstr = adata.var_names.values
+        else:
+            pstr = adata.var[peak_column].values
+        pf = pd.DataFrame([x.replace(":", "-", 1).split("-") for x in pstr], columns=["Chromosome", "Start", "End"], index=adata.var_names)
+        pr = pyranges.from_dict({"Chromosome": pf["Chromosome"], "Start": pf["Start"], "End": pf["End"], "peak_name": pf.index.values})
+    with sw("Calculating overlaps"):
+        ## Once peak ranges are gathered, find intersecting gene bodies:
+        inter_df = pr.join(gr).df.loc[:, ["peak_name", "gene_id"]]
+        inter_df["Distance"] = 0
+        ## Then, find genes with minimum upstream/downstream distance away
+        min_df = pr.join(mingr).df.loc[:, ["peak_name", "gene_id"]]
+        ## diff. is accurate, unless overlapping intervals. Do not need to worry, as duplicates from inter_df will take care
+        diff = pf.loc[min_df["peak_name"].values, ["Start", "Start", "End", "End"]].values.astype(int) - gf.loc[min_df["gene_id"].values, ["Start", "End", "Start", "End"]].values.astype(int)
+        min_df["Distance"] = np.abs(diff).min(1) + 1
+        ## Finally, find distal. Only need nearest gene
+        distance_df = pr.nearest(gr).df.loc[:, ["peak_name", "gene_id", "Distance"]]
+        ## Concat such that 1) prioritized intersections, then 2) minimum distance away, then 3) distal
+        df = pd.concat([inter_df, min_df, distance_df]).drop_duplicates(["peak_name", "gene_id"])
+        df["weight"] = np.exp(-1 - np.abs(df["Distance"]) / 5000. + gf.loc[df["gene_id"].values, "log_gene_scale"].values)
+    with sw("Calculating accessibility"):
+        S = scipy.sparse.csr_matrix((df["weight"].values,
+                                     (pf.index.get_indexer(df["peak_name"].values),
+                                      gf.index.get_indexer(df["gene_id"].values))),
+                                    shape=(pf.shape[0], gf.shape[0]))
+        if layer is not None and layer in adata.layers:
+            X = adata.layers[layer]
+        else:
+            X = adata.X
+        gdata = anndata.AnnData(X.dot(S), obs=adata.obs, var=gf.loc[:, ["gene_id", "gene_name"]], dtype=np.float32)
     gdata.var_names = gdata.var["gene_name"].values
     gdata.var_names_make_unique()
     del gdata.var["gene_name"]

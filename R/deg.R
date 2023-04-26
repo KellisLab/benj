ee4d10e0fbdfe72eb2c1f623913e56a14ba2f370
Benjamin James <benjames@mit.edu>
2023-04-26 00:59:26 -0400

    Convert factors to strings in ruvseq

--- R/deg.R
+++ R/deg.R
@@ -88,6 +88,7 @@ deg.edger <- function(se, pathology, case, control, covariates=c(),
 #' @param norm edgeR norm method for calcNormFactors
 #' @export
 deg.ruvseq <- function(sce, sample, pathology, covariates=NULL, NRUV=3, assay=NULL, cpm.cutoff=10, cpm.frac=0.25, norm="TMM", filter_only_case_control=TRUE) {
+    require(dplyr)
     pb = se_make_pseudobulk(sce, sample)
     if (filter_only_case_control) {
         pb = pb[,SummarizedExperiment::colData(pb)[[pathology]] %in% c(case, control)]
@@ -109,6 +110,7 @@ deg.ruvseq <- function(sce, sample, pathology, covariates=NULL, NRUV=3, assay=NU
     if (!is.character(cd[[pathology]]) | !is.factor(cd[[pathology]])) {
         warning(paste0("Converting ", pathology, " to factor"))
     }
+    cd %>% mutate(across(where(is.factor), as.character)) -> cd
     cd[[pathology]] = as.factor(as.character(cd[[pathology]]))
     dgel = edgeR::DGEList(X, group=cd[[pathology]], remove.zeros=TRUE)
     to_keep = Matrix::rowMeans(edgeR::cpm(dgel) > cpm.cutoff) >= cpm.frac

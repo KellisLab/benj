a0e0701fe740b95cc34bdb56ae4954ff916a480e
Benj <benj@medusa.csail.mit.edu>
2023-04-28 12:05:53 -0400

    Add # gene filtering because scrublet does minimal gene filtering

--- scripts/aggregate_anndata.py
+++ scripts/aggregate_anndata.py
@@ -3,7 +3,7 @@
 def add_cell_cycle_info(adata, cell_cycle):
     return 0
 
-def run(metadata, output, directory=".", sample_key="Sample", cell_cycle=None, gtf=None, min_cells_per_sample:int=30, compression:int=9, use_scrublet:bool=True, qc_vars=[], **kwargs):
+def run(metadata, output, directory=".", sample_key="Sample", cell_cycle=None, gtf=None, min_cells_per_sample:int=30, compression:int=9, min_n_genes:int=0, use_scrublet:bool=True, qc_vars=[], **kwargs):
     import os
     from tqdm.auto import tqdm
     import pandas as pd
@@ -15,6 +15,8 @@ def run(metadata, output, directory=".", sample_key="Sample", cell_cycle=None, g
     tbl = {}
     sw = benj.stopwatch()
     bad = []
+    if use_scrublet:
+        min_n_genes = max(min_n_genes, 3)
     with sw("Reading H5AD files"):
         for sample in tqdm(md.index.values):
             fname = os.path.join(directory, "%s.h5ad" % sample)
@@ -23,6 +25,8 @@ def run(metadata, output, directory=".", sample_key="Sample", cell_cycle=None, g
                 for cn in md.columns:
                     adata.obs[cn] = md.loc[sample, cn]
                 if adata.shape[0] >= min_cells_per_sample:
+                    if "n_genes_by_counts" in adata.obs.columns and min_n_genes > 0:
+                        adata = adata[adata.obs["n_genes_by_counts"] >= min_n_genes, :].copy()
                     tbl[sample] = adata
                 else:
                     bad.append(sample)
@@ -50,6 +54,7 @@ if __name__ == "__main__":
     ap.add_argument("--min-cells-per-sample", type=int, default=30)
     ap.add_argument("--use-scrublet", dest="use_scrublet", action="store_true")
     ap.add_argument("--no-use-scrublet", dest="use_scrublet", action="store_false")
+    ap.add_argument("--min-n-genes", type=int, default=0)
     ap.add_argument("--compression", type=int, default=9)
     ap.set_defaults(scrublet=True)
     args = vars(ap.parse_args())

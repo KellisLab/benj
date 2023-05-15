62657e9b379e0bcbaead316796a5e9003fe45d95
Benjamin James <benjames@mit.edu>
2023-05-15 16:26:32 -0400

    added altair to pybenj; updated jupyter_luria to use exec

--- scripts/jupyter_luria.sh
+++ scripts/jupyter_luria.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -p kellis
 #SBATCH -J jupyter
+### Usage: 'sbatch jupyter_luria.sh envname' on host
+### Usage: 'jupyter_luria.sh b4' on client
 export JUPYTER_LURIA_PREFIX=${JUPYTER_LURIA_PREFIX:-"99"}
 if hostname -s | grep -qE '^b[0-9]+$'; then
     ### we are on a compute node
@@ -9,7 +11,7 @@ if hostname -s | grep -qE '^b[0-9]+$'; then
     echo "Activating Jupyter notebook on (${conda_env}) for port ${PORT} in dir $PWD"
     source ~/src/conda_init.sh
     conda activate ${conda_env}
-    jupyter notebook --no-browser --port=${PORT}
+    exec jupyter notebook --no-browser --port=${PORT}
 else
     PORT=$(echo $1 | tr -d 'b' | xargs printf "${JUPYTER_LURIA_PREFIX}%02d\n")
     ssh ${ARGS} -N -L ${PORT}:localhost:${PORT} "$@"

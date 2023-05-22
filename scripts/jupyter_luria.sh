1b911dbb8133af973fcbe1ea2ec684161e77f390
Benjamin James <benjames@mit.edu>
2023-05-22 11:49:18 -0400

    changed jupyter cmd

--- scripts/jupyter_luria.sh
+++ scripts/jupyter_luria.sh
@@ -4,6 +4,7 @@
 ### Usage: 'sbatch jupyter_luria.sh envname' on host
 ### Usage: 'jupyter_luria.sh b4' on client
 export JUPYTER_LURIA_PREFIX=${JUPYTER_LURIA_PREFIX:-"99"}
+export JUPYTER_CMD="lab"
 if hostname -s | grep -qE '^b[0-9]+$'; then
     ### we are on a compute node
     conda_env=${1:-${CONDA_DEFAULT_ENV}}
@@ -11,7 +12,7 @@ if hostname -s | grep -qE '^b[0-9]+$'; then
     echo "Activating Jupyter notebook on (${conda_env}) for port ${PORT} in dir $PWD"
     source ~/src/conda_init.sh
     conda activate ${conda_env}
-    exec jupyter notebook --no-browser --port=${PORT}
+    exec jupyter lab --no-browser --port=${PORT}
 else
     PORT=$(echo $1 | tr -d 'b' | xargs printf "${JUPYTER_LURIA_PREFIX}%02d\n")
     ssh ${ARGS} -N -L ${PORT}:localhost:${PORT} "$@"

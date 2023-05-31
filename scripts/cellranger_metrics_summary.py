3a76b08b6899c4d64710a6c4e40bd847c5198eb6
Benjamin James <bjames@openmailbox.org>
2023-05-31 14:23:06 -0400

    cellranger metrics_summary aggregator

---
+++ scripts/cellranger_metrics_summary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+if __name__ == "__main__":
+        import argparse
+        import sys, os
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-i", "--input", dest="csv", nargs="+")
+        ap.add_argument("-o", "--output", required=True)
+        ap.add_argument("-s", "--sep", default=",")
+        args = vars(ap.parse_args())
+        out = {}
+        for csv in args["csv"]:
+                outs_path = os.path.dirname(csv)
+                sample_path = os.path.dirname(outs_path)
+                out[os.path.basename(sample_path)] = pd.read_csv(csv, thousands=",")
+        pd.concat(out).reset_index(level=1, drop=True).to_csv(args["output"], sep=sep)

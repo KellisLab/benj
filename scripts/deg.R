c88438b7039b2b3b816771ee406c338ead2493c9
Benjamin James <benjames@mit.edu>
2023-07-26 14:09:04 -0400

    error backtrace for deg.R

--- scripts/deg.R
+++ scripts/deg.R
@@ -1,5 +1,19 @@
 #!/usr/bin/env Rscript
-
+options(error = function() {
+  calls <- sys.calls()
+  if (length(calls) >= 2L) {
+    sink(stderr())
+    on.exit(sink(NULL))
+    cat("Backtrace:\n")
+    calls <- rev(calls[-length(calls)])
+    for (i in seq_along(calls)) {
+      cat(i, ": ", deparse(calls[[i]], nlines = 1L), "\n", sep = "")
+    }
+  }
+  if (!interactive()) {
+    q(status = 1)
+  }
+})
 
 ## 1. get args
 ## 2. get obs

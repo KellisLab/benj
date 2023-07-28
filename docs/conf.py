ed91b41dbc3536a2f8399a5fbf3743a58dee154f
Benjamin James <benjames@mit.edu>
2023-07-28 16:50:37 -0400

    readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,9 @@ release = '0.0.6'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "nbsphinx"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']

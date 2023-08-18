b91bbc86ae1a9723c1e180fdc4f4053ea3696b6c
Your Name <your-email@example.com>
2023-08-18 00:44:15 -0400

    Use 500bp,5k tile matrices, and estimate gene score in ATAC

--- scripts/h5ad_from_archr_annotation.sh
+++ scripts/h5ad_from_archr_annotation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# usage: export PEAKS=/path/to/peaks; export BLACKLIST=/path/to/blacklist; < sample_list.txt xargs -P8 -I{} bash -c 'h5ad_from_archr_annotation.sh {}'
+# usage: export BLACKLIST=/path/to/blacklist; < sample_list.txt xargs -P8 -I{} bash -c 'h5ad_from_archr_annotation.sh {}'
 
 outdir="$1"
 shift;
@@ -12,15 +12,16 @@ else
     sample=$(basename "${outdir}")
 fi
 
-# if [[ -z "${REFDATA}" ]]; then
-#     refdata=$(awk -F'= ' '/reference_path/{gsub(/[" ,]/, "", $2); print $2}' "${outdir}/_invocation")
-# elif [[ -d "${REFDATA}" ]]; then
-#     refdata="${REFDATA}"
-# else
-#     echo "refdata directory must be set, by either having a 10x style _invocation file, or providing the REFDATA env variable."
-#     exit 1
-# fi
-
+if [[ -z "${REFDATA}" ]]; then
+    refdata=$(awk -F'= ' '/reference_path/{gsub(/[" ,]/, "", $2); print $2}' "${outdir}/_invocation")
+elif [[ -d "${REFDATA}" ]]; then
+    refdata="${REFDATA}"
+else
+    echo "refdata directory must be set, by either having a 10x style _invocation file, or providing the REFDATA env variable."
+    exit 1
+fi
+gtf="${refdata}/genes/genes.gtf.gz"
+tss="${refdata}/regions/tss.bed"
 md=$(dirname "${outdir}")
 md="${md}/ArrowFiles/archr_metadata.tsv.gz"
 if [[ ! -f "${md}" ]]; then
@@ -28,14 +29,14 @@ if [[ ! -f "${md}" ]]; then
     exit 1
 fi
 
-
-if [[ -z "${PEAKS}" ]]; then
-    echo "PEAKS must be set and be a file!"
-    exit 1
-elif [[ ! -f "${PEAKS}" ]]; then
-    echo "PEAKS must be set and be a file!"
-    exit 1
-fi
+mytempbed=$(mktemp)
+bed5k=$(mktemp)
+bed500=$(mktemp)
+bedtools makewindows -b <(awk '{ print $1"\t0\t"$2 }' < "${refdata}/star/chrNameLength.txt") -w 5000 > "${mytempbed}"
+Rscript -e "benj::bed.dump(benj::peak_annotation(benj::bed.slurp(\"${mytempbed}\"), \"${refdata}/genes/genes.gtf.gz\"), \"${bed5k}\")"
+bedtools makewindows -b <(awk '{ print $1"\t0\t"$2 }' < "${refdata}/star/chrNameLength.txt") -w 500 > "${mytempbed}"
+Rscript -e "benj::bed.dump(benj::peak_annotation(benj::bed.slurp(\"${mytempbed}\"), \"${refdata}/genes/genes.gtf.gz\"), \"${bed500}\")"
+rm "${mytempbed}"
 
 extraopts=""
 if [[ -f "${outdir}/outs/atac_fragments.tsv.gz" ]]; then
@@ -49,10 +50,33 @@ else
     exit 1
 fi
 
-mkdir -p "H5AD/ATAC"
-output="H5AD/ATAC/${sample}.h5ad"
-if [[ -f "${BLACKLIST}" ]]; then
-    h5ad_from_archr_annotation.py -f "${fragments}" -s "${sample}" --cell-metadata "${md}" --peaks "${PEAKS}" -o "${output}" -b "${BLACKLIST}"
-else
-    h5ad_from_archr_annotation.py -f "${fragments}" -s "${sample}" --cell-metadata "${md}" --peaks "${PEAKS}" -o "${output}"
+mkdir -p "H5AD/TileMatrix5k"
+
+if [[ -f "${bed5k}" ]]; then
+    hdir="H5AD/TileMatrix5k/"
+    mkdir -p "${hdir}"
+    peaks="${bed5k}"
+    if [[ -f "${BLACKLIST}" ]]; then
+	h5ad_from_archr_annotation.py -f "${fragments}" -s "${sample}" --cell-metadata "${md}" --peaks "${peaks}" -o "${hdir}/${sample}.h5ad" -b "${BLACKLIST}"
+    else
+	h5ad_from_archr_annotation.py -f "${fragments}" -s "${sample}" --cell-metadata "${md}" --peaks "${peaks}" -o "${hdir}/${sample}.h5ad"
+    fi
+    gs_in="${hdir}/${sample}.h5ad"
+    rm "${bed5k}"
 fi
+
+if [[ -f "${bed500}" ]]; then
+    hdir="H5AD/TileMatrix500/"
+    peaks="${bed500}"
+    mkdir -p "${hdir}"
+    if [[ -f "${BLACKLIST}" ]]; then
+	h5ad_from_archr_annotation.py -f "${fragments}" -s "${sample}" --cell-metadata "${md}" --peaks "${peaks}" -o "${hdir}/${sample}.h5ad" -b "${BLACKLIST}"
+    else
+	h5ad_from_archr_annotation.py -f "${fragments}" -s "${sample}" --cell-metadata "${md}" --peaks "${peaks}" -o "${hdir}/${sample}.h5ad"
+    fi
+    gs_in="${hdir}/${sample}.h5ad"
+    rm "${bed500}"
+fi
+
+mkdir -p "H5AD/GeneScoreMatrix"
+estimate_gene_accessibility -i "${gs_in}" -o "H5AD/GeneScoreMatrix/${sample}.h5ad" --gtf "${gtf}" --tss "${tss}"

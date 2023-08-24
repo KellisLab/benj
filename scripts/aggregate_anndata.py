47c19f92fcc34ad856ea226536b9c6d7de2e5e8f
Benjamin James <benjames@mit.edu>
2023-08-23 22:14:43 -0400

    Revamped backed aggregation to use run() and benj.utils.read_elems

--- scripts/aggregate_anndata.py
+++ scripts/aggregate_anndata.py
@@ -3,7 +3,7 @@
 def add_cell_cycle_info(adata, cell_cycle):
     return 0
 
-def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gtf=None, min_cells_per_sample:int=30, compression:int=9, min_n_genes:int=0, min_total_counts:int=0, use_scrublet:bool=True, qc_vars=[], **kwargs):
+def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gtf=None, min_cells_per_sample:int=30, compression:int=9, min_n_genes:int=0, min_total_counts:int=0, use_scrublet:bool=True, qc_vars=[], backed:bool=False, **kwargs):
     import os
     from tqdm.auto import tqdm
     import pandas as pd
@@ -13,6 +13,7 @@ def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gt
     import benj
     md = pd.read_csv(metadata, sep="\t", index_col=0)
     tbl = {}
+    file_tbl = {}
     sw = benj.stopwatch()
     bad = []
     if use_scrublet:
@@ -23,7 +24,10 @@ def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gt
             for dname in directory:
                 fname = os.path.join(dname, "%s.h5ad" % sample)
                 if os.path.isfile(fname):
-                    adata = sc.read(fname)
+                    if backed:
+                        adata = anndata.AnnData(**benj.utils.read_elems(fname, ["obs", "var", "obsm", "varm", "obsp", "varp", "uns", "layers"]))
+                    else:
+                        adata = sc.read(fname)
                     if "scrublet" in adata.uns:
                         if "batches" in adata.uns["scrublet"]:
                             scrub_table |= adata.uns["scrublet"]["batches"]
@@ -37,6 +41,7 @@ def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gt
                         adata = adata[adata.obs["total_counts"] >= min_total_counts, :].copy()
                     if adata.shape[0] >= min_cells_per_sample:
                         tbl[sample] = adata
+                        file_tbl[sample] = fname
                     else:
                         bad.append(sample)
                     break
@@ -52,6 +57,12 @@ def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gt
         if len(tk) == len(scrub_table.keys()):
             adata.uns["scrublet"] = {"batches": scrub_table,
                                      "batched_by": "Sample"}
+    if backed:
+        with sw("Loading X (%d, %d)" % adata.shape):
+            from anndata.experimental.multi_files import AnnCollection
+            adata.X = AnnCollection({k: sc.read(v, backed="r") for k, v in file_tbl.items()},
+                                    join_vars="inner")[adata.obs_names, adata.var_names].X
+            del file_tbl
     with sw("Calculating statistics"):
         if qc_vars is None:
             qc_vars=[]
@@ -74,74 +85,6 @@ def run(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gt
             adata.write_h5ad(output, compression="gzip", compression_opts=compression)
             pass
 
-
-def read_backed_noX(fname, min_n_genes:int=0, **kwargs):
-    """Read an AnnData object except for .X, .raw, and .layers"""
-    import h5py
-    import anndata
-    from anndata.experimental import read_elem
-    attrs = ["obs", "obsm", "obsp", "var", "varm", "varp", "uns"]
-    with h5py.File(fname, "r") as f:
-        adata = anndata.AnnData(**{k: read_elem(f[k]) for k in attrs})
-    for cn, val in kwargs.items():
-        adata.obs[cn] = val
-    if "n_genes_by_counts" in adata.obs.columns and min_n_genes > 0:
-        adata = adata[adata.obs["n_genes_by_counts"] >= min_n_genes, :].copy()
-    return adata
-
-def run_backed(metadata, output, directory=[], sample_key="Sample", cell_cycle=None, gtf=None, min_cells_per_sample:int=30, compression:int=9, min_n_genes:int=0, use_scrublet:bool=True, qc_vars=[], **kwargs):
-    import os
-    from tqdm.auto import tqdm
-    import pandas as pd
-    import numpy as np
-    import anndata
-    import scanpy as sc
-    import benj
-    md = pd.read_csv(metadata, sep="\t", index_col=0)
-    tbl = {}
-    sw = benj.stopwatch()
-    bad = []
-    if use_scrublet:
-        min_n_genes = max(min_n_genes, 3)
-    with sw("Finding H5AD files"):
-        for sample in tqdm(md.index.values):
-            for dname in directory:
-                fname = os.path.join(dname, "%s.h5ad" % sample)
-                if os.path.isfile(fname):
-                    import h5py
-                    with h5py.File(fname, "r") as f:
-                        ashape = f["X"].attrs["shape"]
-                    if ashape[0] >= min_cells_per_sample:
-                        tbl[sample] = fname
-                    else:
-                        bad.append(sample)
-                    break
-            else:
-                raise RuntimeError("Sample %s.h5ad did not exist in the directories: %s" % (sample, ",".join(directory)))
-    if bad:
-        print("Bad samples: ", ",".join(bad))
-    with sw("Concatenating H5AD non-X items"):
-        adata = anndata.concat({sample: read_backed_noX(fname, min_n_genes=min_n_genes,
-                                                        **{k: md.loc[sample, k] for k in md.columns})
-                                for sample, fname in tbl.items()},
-                               merge="same", uns_merge="same")
-    with sw("Writing X-less H5AD to disk"):
-        adata.write_h5ad(output, compression="gzip", compression_opts=compression)
-    with sw("Loading X (%d, %d)" % adata.shape):
-        from anndata.experimental.multi_files import AnnCollection
-        adata.X = AnnCollection({k: sc.read(v, backed="r") for k, v in tbl.items()},
-                                join_vars="inner")[adata.obs_names, adata.var_names].X
-        del tbl
-    with sw("Calculating statistics"):
-        if qc_vars is None:
-            qc_vars=[]
-        sc.pp.calculate_qc_metrics(adata, qc_vars=qc_vars, inplace=True, percent_top=[])
-    if use_scrublet:
-        with sw("Scrublet"):
-            sc.external.pp.scrublet(adata, batch_key="Sample")
-    with sw("Writing H5AD"):
-        adata.write_h5ad(output, compression="gzip", compression_opts=compression)
-
 if __name__ == "__main__":
     import argparse
     ap = argparse.ArgumentParser()
@@ -159,7 +102,4 @@ if __name__ == "__main__":
     ap.add_argument("--no-backed", dest="backed", action="store_false")
     ap.set_defaults(use_scrublet=False, backed=False)
     args = vars(ap.parse_args())
-    if args["backed"]:
-        run_backed(**args)
-    else:
-        run(**args)
+    run(**args)

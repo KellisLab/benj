f04b648f007bb011fd6faa757ecf8d2a4cdb82c1
Benjamin James <benjames@mit.edu>
2023-09-11 21:40:00 -0400

    debug matrices

--- R/deg.R
+++ R/deg.R
@@ -102,9 +102,10 @@ deg.dysregulation <- function(sce, pathology, sample.col, covariates=NULL,  verb
     design = design[,-grep("^X.Intercept.$", colnames(design))] ### stopgap
     print(tibble::as_tibble(design), n=nrow(design))
     X1 = limma::removeBatchEffect(X, covariates=design)
-    cat("NAs:", colSums(is.na(X1)))
-    cat("NAs:", rowSums(is.na(X1)))
     MP = make_pseudobulk(cd$Dx)
+    print(str(X1))
+    print(str(MP))
+    print(str(diag(1/colSums(MP))))
     D = X1 %*% MP %*% diag(1/colSums(MP))
     dnum = sum(dist(t(D)))
     return(dnum)

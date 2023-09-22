fc6f21dcb9a7981473a35c2ea3e4f1fd75ae5b87
Benjamin James <benjames@mit.edu>
2023-09-22 10:03:06 -0400

    Have flag for if data is already normalized

--- benj/incrementalpca.py
+++ benj/incrementalpca.py
@@ -1,8 +1,9 @@
 class _RawNormalizeLogScaler:
-        def __init__(self, mean, std, target_sum:int=None):
+        def __init__(self, mean, std, target_sum:int=None, eps=1e-50, pre_normalized:bool=False):
                 self.mean = mean
                 self.std = std
                 self.target_sum = target_sum
+                self.eps = eps
                 if target_sum is None: ### weird python thing
                         self.target_sum = None
         def transform(self, data):
@@ -12,15 +13,18 @@ class _RawNormalizeLogScaler:
                 import scanpy as sc
                 if not isinstance(data, anndata.AnnData):
                         data = anndata.AnnData(data)
-                X = sc.pp.normalize_total(data, target_sum=self.target_sum, inplace=False)["X"]
-                X = sc.pp.log1p(X)
-                X = (X - np.reshape(self.mean, (1, -1))) / np.reshape(self.std, (1, -1))
+                if pre_normalized:
+                        X = data.X
+                else:
+                        X = sc.pp.normalize_total(data, target_sum=self.target_sum, inplace=False)["X"]
+                        X = sc.pp.log1p(X)
+                X = (X - np.reshape(self.mean, (1, -1))) / np.reshape(self.std, (1, -1)).clip(self.eps, np.inf)
                 return np.asarray(X)
 
 class IncrementalPCA:
-        def __init__(self, var, target_sum:int=None, n_components:int=50):
+        def __init__(self, var, target_sum:int=None, n_components:int=50, pre_normalized:bool=False):
                 from .incrementalsvd import IncrementalSVD
-                self.scaler = _RawNormalizeLogScaler(mean=var["mean"], std=var["std"], target_sum=target_sum)
+                self.scaler = _RawNormalizeLogScaler(mean=var["mean"], std=var["std"], target_sum=target_sum, pre_normalized=pre_normalized)
                 self.svd = IncrementalSVD(n_components=n_components)
         def partial_fit(self, X):
                 self.svd.partial_fit(self.scaler.transform(X))

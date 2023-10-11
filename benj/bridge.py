2a3f993df83752b8465db5c1ccede83365bc571d
Benjamin James <benjames@mit.edu>
2023-10-11 16:46:43 -0400

    Add laplacian eigenmap to bridge

--- benj/bridge.py
+++ benj/bridge.py
@@ -11,3 +11,14 @@ class Bridge:
         def transform(self, dim_rep):
                 """Use unimodal (or all) """
                 return dim_rep.dot(self.w)
+
+def Laplacian(matrix, n=50, **kwargs):
+        import numpy as np
+        from scipy.sparse import diags
+        from scipy.sparse.linalg import eigsh
+        D_half_inv = diags(np.ravel(matrix.sum(1))**-0.5)
+        L = -D_half_inv @ matrix @ D_half_inv
+        L.setdiag(1 + L.diagonal())
+        e_vals, e_vecs = eigsh(L, k=n + 1, which="SM", **kwargs)
+        e_vals, e_vecs = e_vals[1:], e_vecs[:, 1:]
+        return e_vals, e_vecs[:, ::-1]

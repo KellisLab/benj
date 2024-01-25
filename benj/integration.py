d80269a8a17da82d71fce78f2f53cb54f968d119
Benjamin James <benjames@mit.edu>
2024-01-25 17:14:12 -0500

    Only dotplot genes in var_names

--- benj/integration.py
+++ benj/integration.py
@@ -229,7 +229,7 @@ def integrate_rna(adata, output=None, batch=None, hvg:int=0, use_combat:bool=Fal
     sc.pl.umap(adata, color=leiden, save="_%s_ondata.png" % leiden, legend_loc="on data", legend_fontsize=2)
     if dotplot is not None:
         if not isinstance(dotplot, str):
-            dotplot = np.ravel([x.split(",") for x in dotplot])
+            dotplot = np.intersect1d(np.ravel([x.split(",") for x in dotplot]), adata.var_names)
         sc.pl.dotplot(adata, var_names=dotplot, groupby=leiden, save="%s.png" % leiden, standard_scale="var")
     for vv in np.intersect1d(["pct_counts_mt", "doublet_score", "log1p_total_counts"], adata.obs.columns):
         sc.pl.violin(adata, vv, groupby=leiden, save="_%s_%s.png" % (leiden, vv))

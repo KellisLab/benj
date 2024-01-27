bc2e81740ffac56ac7d457972e580a5d0471ddc3
Benjamin James <benjames@mit.edu>
2024-01-27 16:09:06 -0500

    installExtra use remotes for seurat

--- R/installExtra.R
+++ R/installExtra.R
@@ -3,21 +3,21 @@
 installExtraPackages <- function(upgrade="never", seurat=TRUE, ...) {
 ### Reinstall seurat
     if (seurat) {
-        devtools::install_github("satijalab/seurat", "seurat5", upgrade=upgrade, ...)
-        devtools::install_github("satijalab/seurat-data", "seurat5", upgrade=upgrade, ...)
-        devtools::install_github("satijalab/seurat-wrappers", "seurat5", upgrade=upgrade, ...)
-        devtools::install_github("stuart-lab/signac", upgrade=upgrade, ...)
-        devtools::install_github("satijalab/azimuth", "seurat5", upgrade=upgrade, ...)
+        remotes::install_github("satijalab/seurat", "seurat5", upgrade=upgrade, ...)
+        remotes::install_github("satijalab/seurat-data", "seurat5", upgrade=upgrade, ...)
+        remotes::install_github("satijalab/seurat-wrappers", "seurat5", upgrade=upgrade, ...)
+        remotes::install_github("stuart-lab/signac", upgrade=upgrade, ...)
+        remotes::install_github("satijalab/azimuth", "seurat5", upgrade=upgrade, ...)
     }
 ### Tools
-    devtools::install_github('lhe17/nebula', upgrade=never, ...)
-    devtools::install_github('cole-trapnell-lab/cicero-release', ref='monocle3', upgrade=never, ...)
-    devtools::install_github('scverse/anndataR', upgrade=never, ...)
-    devtools::install_github('bnprks/BPCells', upgrade=never, ...)
-    devtools::install_github('chrchang/plink-ng', subdir='2.0/pgenlibr', upgrade=never, ...)
-    devtools::install_github('GreenleafLab/ArchR', ref='master', repos=BiocManager::repositories(), upgrade=never, ...)
-    devtools::install_github("caleblareau/gchromVAR", upgrade=never, ...)
-    devtools::install_github("sankaranlab/SCAVENGE", upgrade=never, ...)
+    devtools::install_github('lhe17/nebula', upgrade=upgrade, ...)
+    devtools::install_github('cole-trapnell-lab/cicero-release', ref='monocle3', upgrade=upgrade, ...)
+    devtools::install_github('scverse/anndataR', upgrade=upgrade, ...)
+    devtools::install_github('bnprks/BPCells', upgrade=upgrade, ...)
+    devtools::install_github('chrchang/plink-ng', subdir='2.0/pgenlibr', upgrade=upgrade, ...)
+    devtools::install_github('GreenleafLab/ArchR', ref='master', repos=BiocManager::repositories(), upgrade=upgrade, ...)
+    devtools::install_github("caleblareau/gchromVAR", upgrade=upgrade, ...)
+    devtools::install_github("sankaranlab/SCAVENGE", upgrade=upgrade, ...)
     BiocManager::install("SNPlocs.Hsapiens.dbSNP155.GRCh38")
     ArchR::installExtraPackages()
 }

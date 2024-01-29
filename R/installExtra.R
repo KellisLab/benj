5ca289976aba7be34831eaf751d7568aacb26ad2
Benjamin James <benjames@mit.edu>
2024-01-29 13:30:47 -0500

    Package versions

--- R/installExtra.R
+++ R/installExtra.R
@@ -3,11 +3,18 @@
 installExtraPackages <- function(upgrade="never", seurat=TRUE, ...) {
 ### Reinstall seurat
     if (seurat) {
-        remotes::install_github("satijalab/seurat", "seurat5", upgrade=upgrade, ...)
+        remotes::install_github("satijalab/seurat-object", "v5.0.1", upgrade=upgrade, ...)
+        remotes::install_github("satijalab/seurat", "v5.0.1", upgrade=upgrade, ...)
         remotes::install_github("satijalab/seurat-data", "seurat5", upgrade=upgrade, ...)
         remotes::install_github("satijalab/seurat-wrappers", "seurat5", upgrade=upgrade, ...)
-        remotes::install_github("stuart-lab/signac", upgrade=upgrade, ...)
-        remotes::install_github("satijalab/azimuth", "seurat5", upgrade=upgrade, ...)
+        remotes::install_github("stuart-lab/signac", "1.12", upgrade=upgrade, ...)
+        tryCatch({
+            ### For some reason the github refs/branches do not work
+            remotes::install_github("satijalab/azimuth", "0.5.0", upgrade=upgrade, ...)
+        }, error=function(cond) {
+            print("Using bare Azimuth install")
+            remotes::install_github("satijalab/azimuth", upgrade=upgrade, ...)
+        })
     }
 ### Tools
     devtools::install_github('lhe17/nebula', upgrade=upgrade, ...)

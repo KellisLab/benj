a6d5fdf5c9ef799a3f6c34912c84c2e31af03ca7
Benjamin James <benjames@mit.edu>
2024-08-15 02:57:08 -0400

    another misspelling
    Relief E. Coli Forte Consul Torreon Chan Competitor Beyond Hope
    Amherst Antiviral advisors SSL World News NSCT Baranyi

--- R/go.R
+++ R/go.R
@@ -47,7 +47,7 @@ enrichALL_Excel <- function(xlsx, new_sheet, OrgDb, method,
     meth_path <- paste0(method, "_pathways")
     if (meth_path %in% sheet_names) {
       mpdf <- openxlsx::readWorkbook(wb, sheet=meth_path)
-      if (all.equals(colnames(mpdf), colnames(df))) {
+      if (all.equal(colnames(mpdf), colnames(df))) {
         df <- rbind(mpdf, df)
       } else {
         warning(paste0("Columns do not match:", paste0(colnames(mpdf),collapse=",") ," vs", paste0(colnames(df), collapse=",")))

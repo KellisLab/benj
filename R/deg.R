d0927e5d82ed599d45834ff9a655a15700a65c22
Benjamin James <benjames@mit.edu>
2024-12-05 15:26:43 -0500

    change default to ashr

--- R/deg.R
+++ R/deg.R
@@ -489,7 +489,7 @@ deg.deseq2 <- function(se,
                        control,
                        sample.col,
                        covariates=NULL, independentFiltering=as.logical(Sys.getenv("DESEQ2_INDEPENDENT", "TRUE")=="TRUE"),
-                       shrinkage=Sys.getenv("DESEQ2_SHRINKAGE", "apeglm"),
+                       shrinkage=Sys.getenv("DESEQ2_SHRINKAGE", "ashr"),
                        prefix="DESeq2") {
     pb = calculate_qc_metrics(se_make_pseudobulk(se, sample.col), assay="counts", qc_vars=c("mt", "ribo", "pc", "chrX", "chrY"))
     X = SummarizedExperiment::assays(pb)$counts

092cf56c3555c534786ecddcd163973d2bd03db8
Benjamin James <benjames@mit.edu>
2025-10-02 11:41:15 -0400

    Matrix sum error for dgeMatrix, replace with dense

--- R/sc.R
+++ R/sc.R
@@ -118,7 +118,7 @@ calculate_qc_metrics <- function(se, assay=NULL, log1p=TRUE, qc_vars=c()) {
 ### check if logical
         vals = SummarizedExperiment::rowData(se)[[qcv]]
         if (is.logical(vals)) {
-            tc_qc = setNames(Matrix::colSums(X[vals,]), NULL)
+            tc_qc = setNames(Matrix::colSums(as.matrix(X[vals,,drop=FALSE])), NULL)
             SummarizedExperiment::colData(se)[[paste0("total_counts_", qcv)]] = tc_qc
             SummarizedExperiment::colData(se)[[paste0("pct_counts_", qcv)]] = 100 * tc_qc / cs
             if (log1p) {
